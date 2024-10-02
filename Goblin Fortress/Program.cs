2c2bcff76e411cff8b1a886c4a20c44c38d71b3a
firf0x <140541282+firf0x@users.noreply.github.com>
2024-10-03 01:11:01 +0800

    add begginnig

---
+++ Goblin Fortress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainProgram
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
